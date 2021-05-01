46eed3f950b86da8776c1343ca472683f184cf66
tansing <83265777+tansing@users.noreply.github.com>
2021-04-30 23:34:14 -0400

    Add files via upload

---
+++ PrintLineProject/Lab1/src/Welcome.java
@@ -0,0 +1,11 @@
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Name: Tanvir Singh");
+		System.out.println("ID: 2000333829");
+
+	}
+
+}
